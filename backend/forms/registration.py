c40a56c14b554f8643bcbd8bdff972e28f79c6a1
Валера <val_g_2001@bk.ru>
2023-05-26 17:54:23 +0500

    intermediate: no such file

--- backend/forms/registration.py
+++ backend/forms/registration.py
@@ -3,7 +3,7 @@ from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from backend.models.models import Users
+from backend.models.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -17,11 +17,11 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        user = Users.query.filter_by(username=username.data).first()
+        user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        user = Users.query.filter_by(email=email.data).first()
+        user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
--- backend/models/models.py
+++ backend/models/models.py
@@ -2,7 +2,7 @@ from sqlalchemy.dialects.postgresql import JSON
 from backend import db
 
 
-class Users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
