ee07c813b3ac12680400bc82fd6cc271094aa90a
Dmitry <61654514+Dmitry-Shlenkin@users.noreply.github.com>
2023-05-26 13:35:34 +0500

    er

---
+++ backend/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', '5791628bb0b13ce0c676dfde280ba245')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('FLASK_DB_URL', 'sqlite:///../../database/sqlite.db')
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
+db.app = app
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+login_manager.init_app(app)
+
+with app.app_context():
+    db.create_all()
