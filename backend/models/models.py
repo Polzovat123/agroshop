83f3989e576b644e5b8d47039522dd5680cc7b70
Dmitry <61654514+Dmitry-Shlenkin@users.noreply.github.com>
2023-05-26 16:26:43 +0500

    add models product

--- backend/models/models.py
+++ backend/models/models.py
@@ -27,3 +27,29 @@ class Farmer(db.Model):
 
     def __repr__(self):
         return '<Farmer %r>' % self.farmer_name
+
+
+class Shopping_cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(80), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)  # TODO TIMESTAMP
+    farmer_rating = db.Column(db.Integer(120), nullable=False)
+
+    def __repr__(self):
+        return '<Farmer %r>' % self.farmer_name
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    product_name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    type_id = db.Column(db.Integer(),  nullable=False)
+    tags_id = db.Column(db.Integer(),  nullable=False)
+    date_of_publication = db.Column(db.DateTime, nullable=False)
+    date_update = db.Column(db.DateTime, nullable=False)  # TODO TIMESTAMP
+    image = db.Column(db.String(), nullable=False)
+
+
+    def __repr__(self):
+        return '<Farmer %r>' % self.farmer_name
