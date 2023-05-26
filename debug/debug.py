bf59a9aadaed5444a6b25b609e3db37395c0ba92
useri <useri@LAPTOP-2IUQ8AUK>
2023-05-26 19:56:02 +0500

    - stable version

--- debug/debug.py
+++ debug/debug.py
@@ -1,23 +1,23 @@
 import requests
 
-# params = {'username': '11111111', 'email': 'example@2223324ьж2дло4д23ж4оч.ru', 'password': '1234', 'age':123, 'is_farmer':False}
-# result = requests.post('http://127.0.0.1:5000/register', json=params)
-# print(result.json())
+params = {'username': '11111111', 'email': 'example@2223324ьж2дло4д23ж4оч.ru', 'password': '1234', 'age':123, 'is_farmer':False}
+result = requests.post('http://127.0.0.1:5000/register', json=params)
+print(result.json())
 
 
-# params = {'username': '11111111', 'email': 'example@2223324ьж2дло4д23ж4оч.ru', 'password': '1234', 'age':123, 'is_farmer':False}
-# result = requests.post('http://127.0.0.1:5000/login', json=params)
-# print(result.json())
+params = {'username': '11111111', 'email': 'example@2223324ьж2дло4д23ж4оч.ru', 'password': '1234', 'age':123, 'is_farmer':False}
+result = requests.post('http://127.0.0.1:5000/login', json=params)
+print(result.json())
 
-# result = requests.get('http://127.0.0.1:5000/products')
-# print(result.json())
+result = requests.get('http://127.0.0.1:5000/products')
+print(result.json())
 
-# params = {'product_id': '11111111',
-#           'product_name': 'example@2223324ьж2дло4д23ж4оч.ru',
-#           'description': '1234',
-#           'image': 'path'}
-# result = requests.post('http://127.0.0.1:5000/create_product', json=params)
-# print(result.json())
+params = {'product_id': '11111111',
+          'product_name': 'example@2223324ьж2дло4д23ж4оч.ru',
+          'description': '1234',
+          'image': 'path'}
+result = requests.post('http://127.0.0.1:5000/create_product', json=params)
+print(result.json())
 
 result = requests.get('http://127.0.0.1:5000/delete_product', {'id': 123})
 print(result.json())
