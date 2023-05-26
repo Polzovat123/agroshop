f5f0c3d038aba1dba41ec00b7bdbdd6239ff4c95
Dmitry <61654514+Dmitry-Shlenkin@users.noreply.github.com>
2023-05-26 15:21:19 +0500

    gotovo

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,32 +1,26 @@
 import database
 from sqlalchemy.dialects.postgresql import JSON
+from backend import db
 
 
-class User(database):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False)
-    updated_at = db.Column(db.DateTime(), nullable=False)  # TODO TIMESTAMP
+    updated_at = db.Column(db.DateTime(), nullable=False)
+    age = db.Column(db.Integer(), nullable=False)
+    shop_rating = db.Column(db.Integer(), nullable=False)
+    is_farmer = db.Column(db.Integer(), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
 
-class Role(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.String(80), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<role %r>' % self.role
-
-
 class Farmer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    farmer_name = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    nickname = db.Column(db.String(80), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
     updated_at = db.Column(db.DateTime, nullable=False)  # TODO TIMESTAMP
     farmer_rating = db.Column(db.Integer(120), nullable=False)
