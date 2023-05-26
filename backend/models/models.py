ad6c3672cccbc907f3c2507d20efbb757100777f
Dmitry <61654514+Dmitry-Shlenkin@users.noreply.github.com>
2023-05-26 16:35:54 +0500

    Update models.py

--- backend/models/models.py
+++ backend/models/models.py
@@ -29,7 +29,7 @@ class Farmer(db.Model):
         return '<Farmer %r>' % self.farmer_name
 
 
-class Shopping_cart(db.Model):
+class ShoppingCart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(80), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
@@ -37,19 +37,56 @@ class Shopping_cart(db.Model):
     farmer_rating = db.Column(db.Integer(120), nullable=False)
 
     def __repr__(self):
-        return '<Farmer %r>' % self.farmer_name
+        return '<nickname %r>' % self.nickname
 
 
 class Product(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     product_name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(100), nullable=False)
-    type_id = db.Column(db.Integer(),  nullable=False)
-    tags_id = db.Column(db.Integer(),  nullable=False)
+    type_id = db.Column(db.Integer(), nullable=False)
+    tags_id = db.Column(db.Integer(), nullable=False)
     date_of_publication = db.Column(db.DateTime, nullable=False)
     date_update = db.Column(db.DateTime, nullable=False)  # TODO TIMESTAMP
     image = db.Column(db.String(), nullable=False)
 
+    def __repr__(self):
+        return '<product_name %r>' % self.product_name
+
+
+class ProductTagRelationship(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    product_id = db.Column(db.Integer(), nullable=False)
+    tag_id = db.Column(db.Integer(), nullable=False)
 
     def __repr__(self):
-        return '<Farmer %r>' % self.farmer_name
+        return '<id %r>' % self.id
+
+
+class ShoppingCartProductRelationship(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    shopping_cart_id = db.Column(db.Integer(), nullable=False)
+    product_id = db.Column(db.Integer(), nullable=False)
+
+    def __repr__(self):
+        return '<id %r>' % self.id
+
+
+class ProductTag(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    nametag = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return '<nametag %r>' % self.nametag
+
+
+class ProductType(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    product_type = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return '<id %r>' % self.id
+
+
+
+
